4ac2bd6ab2fc78fe302ea5749f144ea8edf1c6cf
Camila <falaschi.camila@yahoo.com.br>
2022-10-09 00:13:07 -0300

    reqisito 2 - completo

--- src/index.js
+++ src/index.js
@@ -12,15 +12,28 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const pathTalkers = path.resolve(__dirname, '..', 'src', 'talker.json');
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
 app.get('/talker', async (req, res) => {
-  const pathTalkers = path.resolve(__dirname, '..', 'src', 'talker.json');
-  const talker = JSON.parse(await fs.readFile(pathTalkers, 'utf8'));
-  res.status(200).json(talker);
+  const talkerList = JSON.parse(await fs.readFile(pathTalkers, 'utf8'));
+  res.status(200).json(talkerList);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkerList = JSON.parse(await fs.readFile(pathTalkers, 'utf8'));
+  const talker = talkerList.find((item) => item.id === Number(id));
+
+  if (talker) {
+    return res.status(200).json(talker);
+  }
+
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
 app.listen(PORT, () => {
