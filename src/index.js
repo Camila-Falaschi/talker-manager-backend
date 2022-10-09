a8d8fc9eb96d98f9791db8f23c0a0e2f890e6acc
Camila <falaschi.camila@yahoo.com.br>
2022-10-08 23:42:08 -0300

    reqisito 1 - completo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const fs = require('fs').promises;
+const path = require('path');
+
 const app = express();
 app.use(bodyParser.json());
+
+app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -10,6 +15,12 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  const pathTalkers = path.resolve(__dirname, '..', 'src', 'talker.json');
+  const talker = JSON.parse(await fs.readFile(pathTalkers, 'utf8'));
+  res.status(200).json(talker);
 });
 
 app.listen(PORT, () => {
